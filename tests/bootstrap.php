e0da956ad8f30a6eae9e9dd04298995c1419f385
Joost Nijhuis <jnijhuis81@gmail.com>
2021-09-02 09:32:32 +0200

    Upgrade to phpunit ^9.0 Add travis ci builds config and Coveralls code coverage config

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of the jojo1981/polling package
+ *
+ * Copyright (c) 2021 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+DG\BypassFinals::enable();
+
